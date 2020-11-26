c2333c5346bec373005e28463d228f7187cbf541
Mayank-MP05 <mayank5pande@gmail.com>
2020-11-26 14:13:01 +0530

    Login-Signup Working Properly

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from flask_pymongo import PyMongo
 import json 
+import user
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/aimnet"
 mongo = PyMongo(app)
 
-@app.route('/')
-def hello_world():
-    users = mongo.db.users.find()
-    ul = [] 
-    for doc in users:
-        print(doc)
-        ul.append(doc)
-        
-    return jsonify({"li":json.dumps(ul, default=str)})
+@app.route('/login',methods=["POST"])
+def login_route():
+    data = request.get_json()
+    print(data)
+    return user.login(data["email"],data["pass1"],mongo)  
 
-
+@app.route('/signup',methods=["POST"])
+def signup_route():
+    data = request.get_json()
+    print(data)
+    return user.signup(data["email"],data["pass1"],data["pass2"],mongo)   
+    
 if  __name__ == "__main__":
     app.run(debug=True)
---
+++ user.py
@@ -0,0 +1,50 @@
+from flask import jsonify
+import json
+import random
+
+#Login Function
+def login(email,password,mongo):
+    getter = mongo.db.users.find_one({"email":email})
+    #if user is not present in DB
+    if getter is None:
+        return jsonify({
+            "err":f'No user exists with email {email}'
+        })
+    else:
+        if getter["password"] == password:
+            print(getter)
+            # getter._id = None
+            # getter.password = None
+            ul = [] 
+            for doc in getter:
+                print(doc)
+                ul.append(doc)
+            return json.dumps(getter, default=str)
+        else:
+            return jsonify({
+                "err":f'Wrong Password'
+            })
+
+#Signup Function
+def signup(email,pass1,pass2,mongo):
+    dp = random.randint(0,8)
+    if pass1 != pass2:
+        return jsonify({
+            "err":"Password do not match!",
+        })
+    getter = mongo.db.users.insert_one({
+        "name": "",
+        "username": "",
+        "password": pass1,
+        "email": email,
+        "profilePic": dp,
+        "bio": "Music is interesting ..."    
+    }).inserted_id
+    #if user is not present 
+    if getter is None:
+        return jsonify({
+            "err":"Something error occured!"
+        })
+    else:
+        setter = mongo.db.users.find_one({"_id":getter})
+        return json.dumps(setter,default=str)
