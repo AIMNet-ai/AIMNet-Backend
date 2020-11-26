371a0c810f7ce6ef550a69566ef3fd7de86b8245
Mayank-MP05 <mayank5pande@gmail.com>
2020-11-26 12:29:34 +0530

    First Database Test Successful

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask,jsonify
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/aimnet"
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
+    users = mongo.db.users.find()
+    for doc in users:
+        print(doc)
     return 'fuck hi bye World!'
 
 
