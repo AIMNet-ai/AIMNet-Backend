83c631b2b0f0b2b7e28a99fc12740fc52d220307
Mayank-MP05 <mayank5pande@gmail.com>
2020-11-26 12:17:03 +0530

    First ever very basic flask app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("this is AIMNet Backend")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'fuck hi bye World!'
+
+
+if  __name__ == "__main__":
+    app.run(debug=True)
